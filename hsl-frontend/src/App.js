5d0cc6db4e978e944f5af3f72a044c545f09e79c
Tyna William <tynawilliam@gmail.com>
2022-06-26 18:11:52 -0400

    get user data from api

--- hsl-frontend/src/App.js
+++ hsl-frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+  const [userId, setUserId] = useState("")
+  const [isValid, setIsValid] = useState(false)
+
+  const setId = (e) => {
+    setUserId(e.target.value)
+  }
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    async function validateUser() {
+      console.log(userId);
+      const res = await fetch(`/api/users/${userId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      const resData = await res.json();
+      if(!res.ok) {
+        setIsValid(false)
+        console.log("Error")
+      } else {
+        setIsValid(true);
+      }
+    }
+    validateUser()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hi Tyna</h1>
+    <form onSubmit={onSubmit}>
+      <input id="getId" onChange={setId}/>
+      <input type="submit" value="login"/>
+    </form>
     </div>
   );
 }
