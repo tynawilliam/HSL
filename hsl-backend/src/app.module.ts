9836002170a258d0a0f714c0b33caad091a795e9
Tyna William <tynawilliam@gmail.com>
2022-06-26 10:23:43 -0400

    generate prisma files

--- hsl-backend/src/app.module.ts
+++ hsl-backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AnswersModule } from './answers/answers.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AnswersModule, QuestionsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
